e133831e42056e1f6896f7149d7dec2fb4f30c3e
Billy Arnold <billyarnoldd@gmail.com>
2024-06-11 21:47:33 +0100

    starting testing and working on html node

---
+++ src/htmlnode.py
@@ -0,0 +1,25 @@
+class HTMLNode:
+    def __init__(self, tag=None, value=None, children=None, props=None):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+
+    def to_html(self):
+        raise NotImplementedError()
+    
+    def props_to_html(self):
+        string = ""
+
+        for key, value in self.props:
+            string += f"{key}='{value}' "
+
+        return string
+
+    def __repr__(self):
+        print(self.tag)
+        print(self.value)
+        print(self.children)
+        print(self.props)
+
+
---
+++ src/test_htmlnode.py
@@ -0,0 +1,9 @@
+import unittest
+
+from htmlnode import HTMLNode
+
+class TestHTMLNode(unittest.TestCase):
+
+
+if __name__ == "__main__":
+    unittest.main()
