0b8542808c1a768ee5479fc0a16877309859043c
Billy Arnold <billyarnoldd@gmail.com>
2024-06-20 15:20:06 +0100

    .

--- src/main.py
+++ src/main.py
@@ -2,8 +2,6 @@ from textnode import TextNode
 from leafnode import LeafNode
 import re
 
-TextNode("This is a text node", "bold", "https://www.boot.dev")
-
 text_type_text = "text"
 text_type_bold = "bold"
 text_type_italic = "italic"
@@ -151,6 +149,7 @@ def block_to_block_type(block):
         return block_type_heading
     if len(lines) > 1 and lines[0].startswith("```") and lines[-1].startswith("```"):
         return block_type_code
+
     if block.startswith(">"):
         for line in lines:
             if not line.startswith(">"):
@@ -173,4 +172,5 @@ def block_to_block_type(block):
                 return block_type_paragraph
             i += 1
         return block_type_ordered_list
+
     return block_type_paragraph
